77ad19e42c4e35bce4224ed17cba9e3e36c3aba4
MohammadSameerSiddique <siddiquesameer483@gmail.com>
2022-11-19 08:31:05 -0800

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
+const Navbar = () => {
+    const items = useSelector((state) => state.cart);
+    return (
+        <div className='navbar'>
+            <h2 className="logo">eCommerce</h2>
+            <div className='links'>
+                <Link className="navLink" to="/">
+                    Home
+                </Link>
+                <Link className="navLink" to="/cart">
+                    Cart
+                </Link>
+                <span className="cartCount">Cart items: {items.length}</span>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
